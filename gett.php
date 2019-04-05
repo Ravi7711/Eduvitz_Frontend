24569717f9da25e441ea62628521a00f06d08ba4
Ravi7711 <48150036+Ravi7711@users.noreply.github.com>
2019-04-05 12:51:24 +0530

    Get course page of eduvitz

---
+++ gett.php
@@ -0,0 +1,594 @@
+
+
+
+<!doctype html>
+<html>
+<head>
+<title>Contact US</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="chat11.css">
+<!--Start of Tawk.to Script-->
+<script type="text/javascript" src="../js/tawk.js">
+</script>
+<!--End of Tawk.to Script-->
+ <style type="text/css">
+		
+    </style>
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		
+<style>
+
+*{
+margin:0px;
+}
+
+.header1{
+	height:auto;
+	background-color:white;
+	display:inline;
+	float:left;
+}
+
+
+.imglogo{
+	height:100px;
+	width:200px;
+	padding-left:5%;
+}
+
+#menu{
+	height:auto;
+	width:auto;
+	float:right;
+	padding-right:5%;
+}
+
+.header2{
+	margin-top:100px;
+	width:100%;
+	height:auto;
+	background-size:cover;
+	color:white;
+
+}
+
+.head1{
+	margin-top:3%;
+	padding-left:10%;
+}
+
+.head2{
+	margin-top:2%;
+	padding-left:15%;
+}
+
+
+
+
+body{
+	
+	background-image:url(images/e14.jpg);background-size:cover;
+	
+}
+
+.body11{
+	text-align:center;
+	margin-top:10%;
+	text-decoration:none;
+}
+
+.body11:hover{
+	text-decoration:none;
+}
+.body22{
+
+}
+
+.location{
+	width:100%;
+	height:500px;
+	background-color:#B587E3;
+}
+
+.footer{
+	background-color:black;
+	color:white;
+}
+
+
+/**********  floating social icons  *******/
+.social {
+  position: fixed;
+  top: 40%;
+  z-index:1;
+}
+.social ul {
+  padding: 0px;
+  -webkit-transform: translate(-260px, 0);
+  -moz-transform: translate(-260px, 0);
+  -ms-transform: translate(-260px, 0);
+  -o-transform: translate(-260px, 0);
+  transform: translate(-260px, 0);
+}
+.social ul li {
+  display: block;
+  margin: 5px;
+  background-color:#33FFEC;
+  width: 270px;
+  text-align: right;
+  padding: 10px;
+  -webkit-border-radius: 0 30px 30px 0;
+  -moz-border-radius: 0 30px 30px 0;
+  border-radius: 0 30px 30px 0;
+  -webkit-transition: all 1s;
+  -moz-transition: all 1s;
+  -ms-transition: all 1s;
+  -o-transition: all 1s;
+  transition: all 1s;
+}
+.social ul li a{
+	font-size:25px;
+	}
+.social ul li:hover {
+  -webkit-transform: translate(110px, 0);
+  -moz-transform: translate(110px, 0);
+  -ms-transform: translate(110px, 0);
+  -o-transform: translate(110px, 0);
+  transform: translate(110px, 0);
+  background-color:#C996D9;
+}
+.social ul li:hover i {
+  color: #fff;
+  background: red;
+  -webkit-transform: rotate(360deg);
+  -moz-transform: rotate(360deg);
+  -ms-transform: rotate(360deg);
+  -o-transform: rotate(360deg);
+  transform: rotate(360deg);
+  -webkit-transition: all 1s;
+  -moz-transition: all 1s;
+  -ms-transition: all 1s;
+  -o-transition: all 1s;
+  transition: all 1s;
+}
+.social ul li:hover:nth-of-type(1) i {
+	background:#3C599F;
+}
+.social ul li:hover:nth-of-type(2) i {
+	background:#32CCFE;
+}
+.social ul li:hover:nth-of-type(3) i {
+	background:#a444ea;
+}
+.social ul li:hover:nth-of-type(4) i {
+	background:#0085AE;
+}
+.social ul li:hover:nth-of-type(5) i {
+	background:#dd4b39;
+}
+.social ul li:hover:nth-of-type(1) a {
+	color:#3C599F;
+}
+.social ul li:hover:nth-of-type(2) a {
+	color:#32CCFE;
+}
+.social ul li:hover:nth-of-type(3) a {
+	color:#a444ea;
+}
+.social ul li:hover:nth-of-type(4) a {
+	color:#0085AE;
+}
+.social ul li:hover:nth-of-type(5) a{
+	color:#dd4b39;
+}
+.social ul li i {
+  margin-left: 10px;
+  color: #000;
+  background: #fff;
+  padding: 6px 7px;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  font-size: 20px;
+  background: #ffffff;
+ webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -ms-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+}
+.social ul li:last-child i{
+padding:6px 4px;
+}
+	.navbar-default {
+		background-color:transparent;
+		border-color:transparent;
+	}
+	.navbar-header {
+        float: none;
+    }
+    .navbar-toggle {
+        display: block;
+    }
+    .navbar-collapse.collapse {
+        display: none!important;
+    }
+    .navbar-nav {
+       margin: 7.5px 50px 7.5px -15px
+    }
+    .navbar-nav>li {
+        float: none;
+    }
+   
+    .navbar-text {
+        float: none;
+        margin: 15px 0;
+    }
+    /* since 3.1.0 */
+    .navbar-collapse.collapse.in { 
+        display: block!important;
+    }
+    .collapsing {
+        overflow: hidden!important;
+    }
+	
+	.header{
+	height:100px;
+	background-color:lightblue;
+	}
+	
+	::selection {
+    background: #1ccdca;
+    color: #fff;
+    text-shadow: none;
+}
+
+.copyright{
+	height:auto;
+	color:black;
+	}
+	
+.location{
+height:auto;
+width:100%;
+}
+.navbar-header{
+    top: 0;
+    z-index: 30;
+
+</style>
+
+
+<script type="text/javascript">
+
+function validateName() {
+
+  var name = document.getElementById('contact-name').value;
+
+  if(name.length == 0) {
+
+    producePrompt('Name is required', 'name-error' , 'red')
+    return false;
+
+  }
+
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+
+    producePrompt('First and last name, please.','name-error', 'red');
+    return false;
+
+  }
+
+  producePrompt('Valid', 'name-error', 'green');
+  return true;
+
+}
+
+function validatePhone() {
+
+  var phone = document.getElementById('contact-phone').value;
+
+    if(phone.length == 0) {
+      producePrompt('Phone number is required.', 'phone-error', 'red');
+      return false;
+    }
+
+    if(phone.length != 10) {
+      producePrompt('Include area code.', 'phone-error', 'red');
+      return false;
+    }
+
+    if(!phone.match(/^[0-9]{10}$/)) {
+      producePrompt('Only digits, please.' ,'phone-error', 'red');
+      return false;
+    }
+
+    producePrompt('Valid', 'phone-error', 'green');
+    return true;
+
+}
+
+function validateEmail () {
+
+  var email = document.getElementById('contact-email').value;
+
+  if(email.length == 0) {
+
+    producePrompt('Email Invalid','email-error', 'red');
+    return false;
+
+  }
+
+  if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+
+    producePrompt('Email Invalid', 'email-error', 'red');
+    return false;
+
+  }
+
+  producePrompt('Valid', 'email-error', 'green');
+  return true;
+
+}
+
+
+
+function validateForm() {
+  if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+    jsShow('submit-error');
+    producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+    setTimeout(function(){jsHide('submit-error');}, 2000);
+  }
+  else {
+
+  }
+}
+
+function jsShow(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function jsHide(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+
+
+
+function producePrompt(message, promptLocation, color) {
+
+  document.getElementById(promptLocation).innerHTML = message;
+  document.getElementById(promptLocation).style.color = color;
+
+
+}
+
+
+</script>
+
+</head>
+
+<body>
+
+<span class="w3-jumbo w3-hide-small;"> <img src="images/eduvitz_logo.png"" width="200"></img></span>
+
+
+
+
+
+
+<div class="container-fluid">
+		<div class="row">
+		
+		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header navbar-fixed-top">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed"  data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="../index.php"><img src="images/eduvitz_logo.png" alt="no image found" style="width:120px;height:90px; margin-top:-14px;"></a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" style="position: absolute;top:50px;color: rgb(255, 255, 255);z-index: 9999; right: 0px !important;" id="bs-example-navbar-collapse-1">
+					  
+						<ul class="nav navbar-nav navbar-right" >
+								<a href="home.html"> <li><font size="5" color="green">Home</font></li>
+								</a>
+								
+								<a href="about.html"> <li><font size="5"color="green">About us</font></li></a>
+								
+								<a href="gett.php"> <li><font size="5" color="green">Courses</font></li></a>
+								
+								
+								<a href="project.html"> <li><font size="5" color="green">Projects</font></li></a>
+								
+								<a href="services.html"> <li><font size="5" color="green">Services</font></li></a>
+								
+								<a href="gallery.html"> <li><font size="5" color="green">Gallery</font></li></a>
+								
+									
+								<a href="indexlms.html"> <li><font size="5" color="green">LMS Project</font></li></a>
+								
+								<a href="contact.html"> <li><font size="5" color="green">Contact US</font></li></a>
+							</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+			
+		</div>
+			
+			
+  
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-content" style="max-width:1500px"/>
+
+<!-- Header -->
+<div class="w3-opacity">
+<span class="w3-button w3-xxlarge w3-white w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></span> 
+<div class="w3-clear"></div>
+<header class="w3-center w3-margin-bottom">
+  
+</header>
+</div>
+
+<!-- !PAGE CONTENT! -->
+
+
+
+
+
+  <style>
+  
+  body{
+  
+  background-image:url("images/blue.jpg");
+  }
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+	text-color:solid-white;
+	background-image:url("images/blue.jpg");
+}
+th, td {
+    padding: 10px;
+    text-align: left;
+	text-color:solid-white;
+}
+th{
+color:blue;
+}
+table#t01 {
+    width: 100%;    
+    background-color: red;
+	color:red;
+}
+</style>
+</head>
+<body>
+
+
+
+<h2 style="color:red;"><center>New Batches</center>
+ <button class="btn btn-secondary pull-right btn btn-warning"><a href="register.html" style="text-decoration:none;color:Green"><h4>Registration</h4></a></button>
+		</h2>
+				
+				
+
+<br>
+
+<?php
+
+
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("127.0.0.1", "radmin", "admin011","Eduvitz_Student");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+
+
+//mysql selection query
+
+$sql = 'select * from course';
+
+
+
+$query = mysqli_query($link, $sql);
+
+
+
+if(!$query){
+     die ('SQL Error: ' . mysqli_error($link));
+}
+?>
+
+
+
+<table id="t01">
+  <tr>
+    <th>Course</th>
+    <th>Faculty</th> 
+    <th>Date</th>
+	 <th>Time</th>
+	 <th>Duration</th>
+  </tr>
+        
+  
+ <?php
+while ($row = mysqli_fetch_array($query))
+{
+    echo '<tr>
+            <td>'.$row['cname'].'</td>
+            <td>'.$row['faculty'].'</td>
+            <td>'.$row['cdate'].'</td>
+            <td>'.$row['ctime'].'</td>
+            <td>'.$row['duration'].'</td>
+        </tr>';
+}
+echo '
+</table>';
+ 
+// Should we need to run this? read section VII
+mysqli_free_result($query);
+ 
+// Close connection
+mysqli_close($link);
+
+//end mysql
+
+?>
+  </tr>
+  
+</table>
+
+</body>
+</html>
+
+  
+  </div>
+  
+</div>
+		
+ 
+
+</body>
+</html> 
